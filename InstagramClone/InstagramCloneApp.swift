055be6e6a6a77d26f44d4f44d547483e52993d18
brsaca <brsaca@gmail.com>
2023-09-20 20:11:06 -0600

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Brenda Saavedra Cantu on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
