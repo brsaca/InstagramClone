7d145d48ffce296ce3533e82a8764aeebb14f113
brsaca <brsaca@gmail.com>
2023-09-21 13:34:56 -0600

    feat: ui feed screen

---
+++ InstagramClone/Core/Feed/View/FeedCell.swift
@@ -0,0 +1,99 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by Brenda Saavedra Cantu on 21/09/23.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            /// image + username
+            HStack(spacing: 4) {
+                Image("bren")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text("bren")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }
+            .padding(.leading)
+            
+            /// post image
+            Image("bren")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            /// action buttons
+            HStack(spacing: 16) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }
+                .accentColor(.black)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }
+                .accentColor(.black)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                .accentColor(.black)
+                
+                Spacer()
+            }.padding(.leading)
+            
+            /// likes label
+            Text("0 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                .padding(.top, 1)
+            
+            /// caption label
+            HStack{
+                Text("morra")
+                    .fontWeight(.semibold) +
+                Text("This is a test of caption of the image")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.footnote)
+            .padding(.leading, 20)
+            .padding(.top, 1)
+            
+            /// time
+            Text("2d ago")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                .padding(.top, 1)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+struct FeedCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedCell()
+    }
+}
---
+++ InstagramClone/Core/Feed/View/FeedView.swift
@@ -0,0 +1,50 @@
+//
+//  FeedView.swift
+//  InstagramClone
+//
+//  Created by Brenda Saavedra Cantu on 21/09/23.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id:\.self){ post in
+                        FeedCell()
+                    }
+                }
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("instagram-logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 110, height: 32)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "paperplane")
+                            .imageScale(.large)
+                    }
+                    .accentColor(.black)
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack{
+            FeedView()
+        }
+    }
+}
