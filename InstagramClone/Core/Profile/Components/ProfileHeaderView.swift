a8da4fa5d6681ba4c07a9a4d7fadaab9418045f8
brsaca <brsaca@gmail.com>
2023-09-26 15:56:32 -0600

    style: set different button if is currentUser in profileView

--- InstagramClone/Core/Profile/Components/ProfileHeaderView.swift
+++ InstagramClone/Core/Profile/Components/ProfileHeaderView.swift
@@ -19,49 +19,51 @@ struct ProfileHeaderView: View {
     
     var body: some View {
         VStack(spacing: 10){
-            /// pic and stats
-            HStack {
-                UserImage(user: user, size: ProfileImageSize.xLarge)
-                
-                Spacer()
-                
-                HStack(spacing: 8) {
-                    UserStatView(title: "Post", value: 3)
+            if let user = self.user {
+                /// pic and stats
+                HStack {
+                    UserImage(user: user, size: ProfileImageSize.xLarge)
                     
-                    UserStatView(title: "Followers", value: 5)
+                    Spacer()
                     
-                    UserStatView(title: "Following", value: 20)
+                    HStack(spacing: 8) {
+                        UserStatView(title: "Post", value: 3)
+                        
+                        UserStatView(title: "Followers", value: 5)
+                        
+                        UserStatView(title: "Following", value: 20)
+                    }
+                }
+                .padding(.horizontal)
+                
+                /// name and bio
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(user.fullname ?? "")
+                        .fontWeight(.semibold)
+                    
+                    Text(user.bio ?? "")
+                }
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+                
+                /// action button
+                Button {
+                    
+                } label: {
+                    Text(user.isCurrentUser ? "Edit Profile" : "Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 32)
+                        .background(user.isCurrentUser ? .white : Color(.systemBlue))
+                        .foregroundColor(user.isCurrentUser ? .black : .white)
+                        .overlay(
+                            RoundedRectangle(
+                                cornerRadius: 6)
+                            .stroke(user.isCurrentUser ? Color.gray : .clear, lineWidth: 1 )
+                        )
                 }
             }
-            .padding(.horizontal)
-            
-            /// name and bio
-            VStack(alignment: .leading, spacing: 4) {
-                Text(user?.fullname ?? "")
-                    .fontWeight(.semibold)
-                
-                Text(user?.bio ?? "")
-            }
-            .font(.footnote)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal)
-            
-            /// action button
-            Button {
-                
-            } label: {
-                Text("Edit Profile")
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                    .frame(width: 360, height: 32)
-                    .foregroundColor(.black)
-                    .overlay(
-                        RoundedRectangle(
-                            cornerRadius: 6)
-                        .stroke(Color.gray, lineWidth: 1)
-                    )
-            }
-            
             Divider()
         }
     }
